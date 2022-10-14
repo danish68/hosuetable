24a7c19784a4f2fd174305ef3bb34d630825738b
bilal ashraf <ashrafbilal862@gmail.com>
2022-10-14 12:37:54 +0500

    routes description added

--- routes/appointment.ts
+++ routes/appointment.ts
@@ -9,6 +9,7 @@ import {
 } from "../controllers/appointment";
 import { Validate } from "../middleware/validator";
 const router = express.Router();
+
 router.get("/all", getAllAppointment);
 router.get("/bill/:petId/", getRemainingBillForPatient);
 router.post("/new", Validate("appointment"), createAppointment);
@@ -17,3 +18,15 @@ router.delete("/", deleteAppointment);
 router.get("/report", getReport);
 
 export default router;
+
+//For All Appointments  :[GET] http://localhost:3000/appointment/all
+//For All Appointments for paid or unpaid : [GET] http://localhost:3000/appointment/all?paid=true
+//For All Appointments for specific date  :[GET] http://localhost:3000/appointment/all?date=2022-10-09T10:10:10:000Z
+//For All Appointments for specific pet :[GET] http://localhost:3000/appointment/all?petId=43w45353f33535
+//For Remaining Bill for specific pet :[GET] http://localhost:3000/appointment/bill/343353535646
+//For Creating new Appointment :[POST] http://localhost:3000/appointment/new
+//For Deleting new Appointment :[DELETE] http://localhost:3000/appointment?id=4335353635r353
+//For Updating new Appointment :[PUT] http://localhost:3000/appointment?id=4553535353
+//For Creating new Appointment :[POST] http://localhost:3000/appointment
+//Get Default Report (includes Total Hospital Paid Balance,Current Month and week paid and unpaid amount):[GET] http://localhost:3000/appointment/report
+//Get Report of all pets with money paid against them and pet with most amount will be popular :[GET] http://localhost:3000/appointment/report?type="moneyByPet"
